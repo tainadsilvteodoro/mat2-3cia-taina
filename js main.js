a1671445a22e5f44601075bc5dc65bde9e23979f
tainadsilvteodoro <114666290+tainadsilvteodoro@users.noreply.github.com>
2024-04-25 16:51:17 -0300

    anna

--- js main.js
+++ js main.js
@@ -13,7 +13,6 @@ for (let i = 0; i < botoes.length; i++) {
           textos[i].classList.add("ativo");
     }
 }
-
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
@@ -21,7 +20,6 @@ const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
